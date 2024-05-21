6596f399235921ce5690cb69ff016343218adb40
Thiago Teofilo <thiago.teophilos@gmail.com>
2024-05-21 17:25:02 -0300

    fix: add PORT on .env

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -31,6 +34,6 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup("api", app, document);
 
-	await app.listen(3000);
+	await app.listen(process.env.PORT);
 }
 bootstrap();
